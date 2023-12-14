0586d707e1ee1d4f8b753ea59e124c88ab258405
Lawrence Forooghian <lawrence@forooghian.com>
2023-12-14 11:29:39 -0300

    Upload a link to the GitHub job
    Currently, the observability server UI links to the workflow run attempt
    that the upload corresponds to. This is fine in the case where the
    workflow only contains a single job, as it did in ably-cocoa. But in the
    case where there’s more than one job in a workflow (e.g. in the case of
    a matrix build), there’s no way to tell which job an upload corresponds
    to. So, include this information in the upload.
    
    Resolves #18.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ An action to push JUnit files to Ably's test observability server.
 - `server-url`: the server to publish results to - see [action.yml](./action.yml) for the default value
 - `server-auth`: authentication key for the server. The `ably` GitHub organization has an org-wide `TEST_OBSERVABLILITY_SERVER_AUTH_KEY` secret. It is recommended that you [make this secret available to your repository](https://docs.github.com/en/actions/using-workflows/sharing-workflows-secrets-and-runners-with-your-organization#sharing-secrets-within-an-organization) and then use this secret as the `server-auth` input.
 - `path`: where to look for `*.junit` files
+- `github-token` (optional): A GitHub access token. If provided, the action will perform a GitHub API call in order to discover the web URL for the current job, and will include this URL in the observability server upload. If the repository is private you must use an access token with the `repo` scope.
+- `job-index` (optional): The index to which the current job corresponds in the response from the ["list jobs for a workflow run attempt" GitHub API](https://docs.github.com/en/rest/actions/workflow-jobs?apiVersion=2022-11-28#list-jobs-for-a-workflow-run-attempt). See the [sample workflow](./.github/workflows/check.yml) for an example of how to calculate this index. If you specify `github-token` but not `job-index`, and the response from this API contains more than one job, the action will fail.
 
 ## Example
 
@@ -19,4 +21,5 @@ Workflow step:
   with:
     server-auth: ${{ secrets.TEST_OBSERVABLILITY_SERVER_AUTH_KEY }}
     path: '.'
+    github-token: ${{ secrets.GITHUB_TOKEN }}
 ```
--- index.ts
+++ index.ts
@@ -5,6 +5,68 @@ import fs from 'fs';
 import path from 'path';
 import {got} from 'got';
 
+/**
+ * Fetches the currently-running job from the GitHub API,
+ * so that we can add a link to it in the upload.
+ *
+ * It’s a bit surprising that there’s no built-in functionality
+ * for this (see e.g. https://stackoverflow.com/questions/71240338/obtain-job-id-from-a-workflow-run-using-contexts
+ * or https://github.com/orgs/community/discussions/8945).
+ */
+async function fetchJob() {
+  const githubToken = core.getInput('github-token');
+  if (!githubToken.length) {
+    return null;
+  }
+
+  const octokit = github.getOctokit(githubToken);
+  const response = await octokit.rest.actions.listJobsForWorkflowRunAttempt(
+      {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        run_id: github.context.runId,
+        attempt_number: parseInt(process.env.GITHUB_RUN_ATTEMPT!),
+      },
+  );
+
+  const jobs = response.data.jobs;
+
+  const jobIndexInput = core.getInput('job-index');
+
+  if (jobs.length > 1 && !jobIndexInput.length) {
+    throw new Error(
+        `Got ${jobs.length} jobs from GitHub API \
+         but don’t know which one to pick. You need to \
+         provide a \`job-index\` input.`,
+    );
+  }
+
+  let jobIndex: number;
+
+  if (jobIndexInput.length) {
+    jobIndex = parseInt(jobIndexInput);
+
+    if (jobIndex >= jobs.length) {
+      throw new Error(
+          `The \`job-index\` input has value ${jobIndex}, \
+          but there are only ${jobs.length} jobs. This \
+          action does not currently handle pagination.`);
+    }
+  } else {
+    if (jobs.length === 0) {
+      throw new Error(
+          'The GitHub API response contains no jobs.',
+      );
+    }
+
+    jobIndex = 0;
+  }
+
+  const job = jobs[jobIndex];
+
+  return job;
+}
+
 // eslint-disable-next-line require-jsdoc
 async function main() {
   const auth = core.getInput('server-auth', {required: true});
@@ -22,6 +84,7 @@ async function main() {
     );
   }
 
+  const job = await fetchJob();
 
   for (const [i, file] of results.entries()) {
     const data = fs.readFileSync(file);
@@ -44,6 +107,8 @@ async function main() {
       iteration: i + 1,
       github_base_ref: process.env.GITHUB_BASE_REF || null,
       github_head_ref: process.env.GITHUB_HEAD_REF || null,
+      github_job_api_url: job?.url ?? null,
+      github_job_html_url: job?.html_url ?? null,
     };
 
 
